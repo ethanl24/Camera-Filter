08ce345ef9b36bc0874ed8235eedcbabf1ffbaa3
ethanl24 <126193084+ethanl24@users.noreply.github.com>
2023-03-17 09:45:09 -0700

    Initial Commit

---
+++ CameraSwiftUI/CameraSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraSwiftUIApp.swift
+//  CameraSwiftUI
+//
+//  Created by Ethan Lee on 3/16/23.
+//
+
+import SwiftUI
+
+@main
+struct CameraSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
